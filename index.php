1dd62cb9159fe5469ff9a7d06b2ea6633bde3d80
Matheus Oberziner <matheus.oberziner@unidavi.edu.br>
2022-09-15 20:00:22 -0300

    .

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php
     include "conexao.php";
-
-    echo 'teste';
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Filmes</title>
+</head>
+<body>
+    <div>
+        <h1>Lista de Filmes</h1>
+        <?php
+            $sql = "SELECT * FROM filmes";
+            $filmes_prepare = $conn->prepare($sql);
+            $filmes_prepare->execute();
+
+            while ($filme = $filmes_prepare->fetch()) {
+                print_r($filme);
+            }
+        ?>
+    </div>
+</body>
+</html>
